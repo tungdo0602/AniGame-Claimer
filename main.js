d621c5063b0e5fad01a31d32f6251ac1f39181c9
Tung <89498812+tungdo0602@users.noreply.github.com>
2025-01-24 18:16:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const prompt = require("readline-sync").question;
 const TOKEN = prompt("Token: ");
 const BOT_ID = "571027211407196161";
 
-const client = new bot.Client();
-
 const rarities = {
     "": 0,
     "C": 1,
@@ -27,7 +25,8 @@ function getRarity(t){
     return "";
 }
 
-let CHANNEL_ID = "";
+let CHANNEL_ID = "",
+    DEBUG_CHANNEL_ID = "";
 let accard = true,
     acgift = true
 
@@ -45,44 +44,52 @@ function updateClaim(textArr){
     return cardclaim;
 }
 
+const client = new bot.Client();
+
 client.on("ready", async () => {
     console.clear();
     console.log(`${client.user.username} is ready!`);
 });
 
-client.on("messageCreate", async function(msg){
-    if(msg.author.id == BOT_ID && msg.channelId == CHANNEL_ID){
-        if(msg.components.length > 0){
-            var btn = msg.components[0].components[0];
-            if(btn.type == "BUTTON"){
-                if(btn.label == "Claim!" && ((new Date()).getTime()/1000) > claimtimestamp && accard){
-                    try {
-                        await msg.clickButton(btn.customId);
+client.on("messageCreate", function(msg){
+    if(msg.author.id == BOT_ID){
+        if(msg.channelId == CHANNEL_ID){
+            if(msg.components.length > 0){
+                var btn = msg.components[0].components[0];
+                if(btn.type == "BUTTON"){
+                    if(btn.label == "Claim!" && ((new Date()).getTime()/1000) > claimtimestamp && accard){
+                        msg.clickButton(btn.customId).catch(()=>{
+                            console.log("Failed to claim card!");
+                            cards -= 1;
+                        });
                         cards += 1;
-                    } catch {}
-                } else if(btn.emoji && btn.emoji.name == "🎁" && acgift){
-                    msg.clickButton(btn.customId);
-                    gifts += 1;
+                    } else if(btn.emoji && btn.emoji.name == "🎁" && acgift){
+                        msg.clickButton(btn.customId).catch(()=>{
+                            console.log("Failed to claim gift!");
+                            gifts -= 1;
+                        });
+                        gifts += 1;
+                    }
                 }
             }
-        }
-        if(msg.embeds.length > 0){
-            var status = msg.embeds[0];
-            var title = status.title;
-            var desc = status.description;
-            if(title.includes(client.user.username) && title.includes("claimed")){
-                if(desc.includes("collection")){
-                    var texts = desc.split("**");
-                    console.log(`Claimed ${updateClaim(texts)}!`);
-                } else if(desc.includes("rewards")){
-                    var texts = desc.split("\n")[1].slice(5).split(" ");
-                    console.log(`Claimed ${updateClaim(texts)}! [gift box]`);
+            if(msg.embeds.length > 0){
+                var status = msg.embeds[0];
+                var title = status.title;
+                var desc = status.description;
+                if(title.includes(client.user.username) && title.includes("claimed")){
+                    if(desc.includes("collection")){
+                        var texts = desc.split("**");
+                        console.log(`Claimed ${updateClaim(texts)}!`);
+                    } else if(desc.includes("rewards")){
+                        var texts = desc.split("\n")[1].slice(5).split(" ");
+                        console.log(`Claimed ${updateClaim(texts)}! [gift box]`);
+                    }
+                } else if(desc && desc.includes("You are not eligible to claim this card!")){
+                    claimtimestamp = Number(desc.split(":")[1]);
+                    console.log("Updated claim cooldown timestamp!");
                 }
-            } else if(desc && desc.includes("You are not eligible to claim this card!")){
-                claimtimestamp = Number(desc.split(":")[1]);
-                console.log("Updated claim cooldown timestamp!");
             }
-        }
+        } else if(msg.channelId == DEBUG_CHANNEL_ID) console.log(msg);
     }
     if(msg.author.id == client.user.id){
         if(msg.content == ".sum"){ // too lazy to use switch case
@@ -93,7 +100,7 @@ client.on("messageCreate", async function(msg){
 > - **Gifts:** \`${gifts}\`
 > -# **Highest rariry:** \`${chighest ? chighest : " "}\`
 > ## Info
-> **Current channel:** <#${CHANNEL_ID}>
+> **Current channel:** ${CHANNEL_ID ? `<#${CHANNEL_ID}>` : "None"}
 > **canClaimCard:** \`${canClaimCard}\`
 > **Claim cooldown:** ${canClaimCard ? "None" : `<t:${claimtimestamp}:F>`}
 ## History (Only SR, UR)
@@ -110,6 +117,10 @@ client.on("messageCreate", async function(msg){
             msg.delete();
             CHANNEL_ID = msg.channel.id;
             msg.channel.send(`> Set channel to <#${CHANNEL_ID}>`);
+        } else if(msg.content == ".dsch"){
+            msg.delete();
+            DEBUG_CHANNEL_ID = msg.channel.id;
+            msg.channel.send(`> Set debug channel to <#${DEBUG_CHANNEL_ID}>`);
         } else if(msg.content == ".ahelp"){
             msg.delete();
             msg.channel.send(`> ## Command list
